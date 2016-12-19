b4b2d51f183319d0987f8ea0a2c342b22cf95539
zhze <zhze93@qq.com>
2016-12-19 13:27:37 +0800

    add email.py

---
+++ email.py
@@ -0,0 +1,24 @@
+#-*- coding:utf-8 -*-
+
+from threading import Thread
+from flask import current_app, render_template
+from flask_mail import Message
+from . import mail
+
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+
+def send_email(to, subject, template, **kwargs):
+    app = current_app._get_current_object()#_get_current_object获得当前(程序)实例。由于程序实例只在运行时创建，
+                                           #无法直接导入(即from . import app)
+                                           #需要用current_app全局变量（单线程）来引入,
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
+                  sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
